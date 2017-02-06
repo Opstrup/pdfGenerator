d8c6b15f697f3a3ddc47a1e049792f23a72145f3
Anders Opstrup <aop@it-minds.dk>
2017-02-06 17:31:39 +0100

    added dummy endpoint to controller for pdf generation

--- app/Http/Controllers/PDFGeneratorController.php
+++ app/Http/Controllers/PDFGeneratorController.php
@@ -3,13 +3,13 @@
 namespace pdfgenerator\Http\Controllers;
 
 use Illuminate\Auth\Access\Response;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
 class PDFGeneratorController extends Controller
 {
     public function dispatchGeneratorTask(Response $response)
     {
+        // dispatch request to correct queue
         $snappy = App::make('snappy.pdf');
         $html = '<h1>Bill</h1><p>You owe me money, dude.</p>';
         return $response->message(
@@ -22,18 +22,12 @@ class PDFGeneratorController extends Controller
         );
     }
 
-    public function test(Response $response)
+    public function test()
     {
         $snappy = App::make('snappy.pdf');
-        $snappy->generateFromHtml('<h1>Bill</h1><p>You owe me money, dude.</p>', '/tmp/bill-123.pdf');
+        $path = __DIR__ . '../../../../temp/';
+        $fileName = time() . '.pdf';
+        $snappy->generateFromHtml('<h1>Bill</h1><p>You owe me money, dude.</p>', $path . $fileName);
         return 'pdf created';
-        /*return $response->message(
-            $snappy->getOutputFromHtml($html),
-            200,
-            array(
-                'Content-Type'          => 'application/pdf',
-                'Content-Disposition'   => 'attachment; filename="file.pdf"'
-            )
-        );*/
     }
 }
