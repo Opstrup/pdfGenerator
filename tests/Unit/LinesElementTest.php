26d0dbbf546cdc46d62359d8d12e31265da32f88
Anders Opstrup <aop@it-minds.dk>
2017-02-15 17:08:27 +0100

    added tests for lines element

--- tests/Unit/LinesElementTest.php
+++ tests/Unit/LinesElementTest.php
@@ -10,7 +10,7 @@ class LinesElementTest extends TestCase
     public function test_element_should_be_empty_table()
     {
         $UUT = new LinesElement([""], [""], "", "");
-        $element = '<table class="table " style=";"> </table>';
+        $element = '<table class="table " style=";"><tr><th></th></tr><tr><td></td></tr></table>';
         $this->assertEquals($element, $UUT->toString());
     }
 
@@ -18,15 +18,39 @@ class LinesElementTest extends TestCase
     {
         $classes = ["table-bordered"];
         $UUT = new LinesElement($classes, [""], "", "");
-        $element = '<table class="table table-bordered" style=";"> </table>';
+        $element = '<table class="table table-bordered" style=";"><tr><th></th></tr><tr><td></td></tr></table>';
         $this->assertEquals($element, $UUT->toString());
     }
 
     public function test_should_create_table_with_data()
     {
-        $data =
         $UUT = new LinesElement([""], [""], "", "");
-        $element = '<table class="table table-bordered" style=";"> </table>';
+        $element = '<table class="table " style=";">' .
+                '<tr>' .
+                    '<th>article number</th>' .
+                    '<th>name</th>' .
+                    '<th>discount</th>' .
+                    '<th>price</th>' .
+                '</tr>' .
+                '<tr>' .
+                    '<td>0132</td>' .
+                    '<td>Sko</td>' .
+                    '<td>0</td>' .
+                    '<td>300</td>' .
+                '</tr>' .
+                '<tr>' .
+                    '<td>3210number</td>' .
+                    '<td>Bukser</td>' .
+                    '<td>20</td>' .
+                    '<td>100</td>' .
+                '</tr>' .
+                '<tr>' .
+                    '<td>7890</td>' .
+                    '<td>T-shirt</td>' .
+                    '<td>50</td>' .
+                    '<td>350</td>' .
+                '</tr>' .
+            '</table>';
         $this->assertEquals($element, $UUT->toString());
     }
 }
