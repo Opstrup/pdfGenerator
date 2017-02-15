d762f49ae0f51f7e07501ac55f5e24dd66154de4
Anders Opstrup <aop@it-minds.dk>
2017-02-15 15:46:19 +0100

    changed generator test to create a table

--- app/Util/PDFGenerator.php
+++ app/Util/PDFGenerator.php
@@ -16,7 +16,7 @@ class PDFGenerator
         $snappy = App::make('snappy.pdf');
         $path = __DIR__ . '/../../temp/';
         $fileName = time() . '.pdf';
-        $snappy->generateFromHtml('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">' .
+        /*$snappy->generateFromHtml('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">' .
             '<div class="container">' .
             '<div class="row">' .
             '<div class="col-xs-12">' .
@@ -30,6 +30,36 @@ class PDFGenerator
             '</div>' .
             '</div>' .
             '<button class="btn btn-default">Test</button>' .
+            '</div>', $path . $fileName);*/
+
+        $snappy->generateFromHtml('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">' .
+            '<div class="container">' .
+            '<table class="table table-condensed">' .
+                '<tr>' .
+                    '<th>article number</th>' .
+                    '<th>name</th>' .
+                    '<th>discount</th>' .
+                    '<th>price</th>' .
+                '</tr>' .
+                '<tr>' .
+                    '<td>0132</td>' .
+                    '<td>Sko</td>' .
+                    '<td>0</td>' .
+                    '<td>300</td>' .
+                '</tr>' .
+                '<tr>' .
+                    '<td>3210number</td>' .
+                    '<td>Bukser</td>' .
+                    '<td>20</td>' .
+                    '<td>100</td>' .
+                '</tr>' .
+                '<tr>' .
+                    '<td>7890</td>' .
+                    '<td>T-shirt</td>' .
+                    '<td>50</td>' .
+                    '<td>350</td>' .
+                '</tr>' .
+            '</table>' .
             '</div>', $path . $fileName);
     }
 
